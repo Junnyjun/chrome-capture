3db619a4b1aedddaa9e7854986b95484e4ed62a9
junnyJun <chbe5082@naver.com>
2023-10-16 15:32:15 +0900

    move

---
+++ external/js/popup.js
@@ -0,0 +1,8 @@
+import model from 'external/js/model.js'
+
+const a = () => {
+  let model =
+  document.getElementById("hide").value = !document.getElementById("hide").value
+  console.log("Hello World")
+  return ''
+}
