d17189f701371f015dbdf7038610b71c8b27cbb7
JunnyJun <chbe5082@naver.com>
2023-10-24 20:47:48 +0900

    capture

--- board-kt/myapp/src/main/kotlin/Main.kt
+++ board-kt/myapp/src/main/kotlin/Main.kt
@@ -1,8 +1,21 @@
+import java.awt.Rectangle
+import java.awt.Robot
+import java.io.File
+import javax.imageio.ImageIO
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
-    Robot()
+    var robot = Robot()
+    val createScreenCapture = robot.createScreenCapture(Rectangle(0, 0, 100, 100))
+    val createMultiResolutionScreenCapture = robot.createMultiResolutionScreenCapture(Rectangle(0, 0, 100, 100))
+
+    val write = ImageIO.write(createScreenCapture, "jpg", File("test.jpg"))
+
+//    val write2 = ImageIO.write(createMultiResolutionScreenCapture, "jpg", File("test2.jpg"))
+    println("robot = ${robot}")
+
 }
